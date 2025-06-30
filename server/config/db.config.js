63c5f581549357ad63a483965633f307adfb19a5
Amar Dhillon <87444806+AmarDhillon05@users.noreply.github.com>
2025-06-30 00:53:52 -0400

    Update db.config.js

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI || "mongodb+srv://terp1:terp2@cluster0.pt7nh.mongodb.net/Echelon?retryWrites=true&w=majority&appName=Cluster0&ssl=true");
+    await mongoose.connect(process.env.DB_URI);
     console.log("Database is running!");
   } catch (e) {
     console.log("Error: " + e.message);
